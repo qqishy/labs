c3d7c4ecab6cf4366febd0e9509d351dbebfe211
qqishy <vnnv2005@mail.ru>
2024-09-17 00:14:01 +0300

    lab2(2)

--- lab2/лаба2/лаба2.cpp
+++ lab2/лаба2/лаба2.cpp
@@ -3,85 +3,97 @@
 #include <time.h>
 #include <iostream>
 
+void shell(int* a, int size) {
+	int gap, x, j;
+	for (int k = 0; k < size; k++) {
+		gap = a[k];
+		for (int i = gap; i < size; ++i) {
+			x = a[i];
+			j = i - gap;
+			while (j >= 0 && x < a[j]) {
+				a[j + gap] = a[j];
+				j -= gap;
+			}
+			a[j + gap] = x;
+		}
+	}
+}
+
+void qs(int* a, int left, int right) {
+	int i, j;
+	int x, y;
+
+	i = left; j = right;
+
+	x = a[(left + right) / 2];
+
+	do {
+		while ((a[i] < x) && (i < right)) i++;
+		while ((x < a[j]) && (j > left)) j--;
+
+		if (i <= j) {
+			y = a[i];
+			a[i] = a[j];
+			a[j] = y;
+			i++; j--;
+		}
+	} while (i <= j);
+
+	if (left < j) qs(a, left, j);
+	if (i < right) qs(a, i, right);
+}
+
+int compare(const void* val1, const void* val2) {
+	return (*(int*)val1 - *(int*)val2);
+};
+
 int main(void)
 {
 	setvbuf(stdin, NULL, _IONBF, 0);
 	setvbuf(stdout, NULL, _IONBF, 0);
 
 	clock_t start, end; // объявляем переменные для определения времени выполнения
-	int** matr;
+	
 
-	const int size = 1000;
+	const int size = 25000;
 
+	int* a = (int*)malloc(size * sizeof(int));
 
-	int** a = (int**)malloc(size * sizeof(int*));
+	
+		
+
+	/*for (int i = 0; i < size; i++) {
+		a[i] = rand() % 100 - 1;
+	}*/
+	/*
+	for (int i = 0; i < size; i++) {
+		a[i] = i;
+	}*/
+	
+	/*
+	for (int i = 0; i < size; i++) {
+		a[i] = size - i;
+	}*/
+	
 	
 	for (int i = 0; i < size; i++) {
-
-		a[i] = (int*)malloc(size * sizeof(int));
-
+		if (i < size / 2) {
+			a[i] = i; //последовательно
+		}
+		else {
+			a[i] = size - i; //убывающая
+		}
 	}
 	
-	int** b = (int**)malloc(size * sizeof(int*));
 
-	for (int i = 0; i < size; i++) {
-
-		b[i] = (int*)malloc(size * sizeof(int));
-
-	}
-
-	int** c = (int**)malloc(size * sizeof(int*));
-
-	for (int i = 0; i < size; i++) {
-
-		c[i] = (int*)malloc(size * sizeof(int));
-
-	}
-		
-		
-	int i = 0, j = 0, r;
-
-	//int a[400][400], b[400][400], c[400][400], elem_c;
-	int elem_c;
-
-
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	while (i < size)
-	{
-		while (j < size)
-		{
-			a[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	i = 0; j = 0;
-	while (i < size)
-	{
-		while (j < size)
-		{
-			b[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
 	start = clock();
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			elem_c = 0;
-			for (r = 0; r < size; r++)
-			{
-				elem_c = elem_c + a[i][r] * b[r][j];
-				c[i][j] = elem_c;
-			}
-		}
-	}
+	/*shell(a, size);*/
+	/*qs(a, 0, size - 1);*/
+	qsort(a, size, sizeof(int), compare);
 	end = clock();
-	/*std::cout << float(start) / float(CLOCKS_PER_SEC);*/
-	std::cout << (float(end) - float(start)) / float(CLOCKS_PER_SEC);
 
+	std::cout << float(end - start) / CLOCKS_PER_SEC << std::endl;
+
+	free(a);
 	return(0);
 }
