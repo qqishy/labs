983bed425c799cc58c605c5a79998c2e5bc2c27a
qqishy <vnnv2005@mail.ru>
2024-09-17 15:36:50 +0300

    1l(dop)

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -1,123 +1,70 @@
 ﻿#include <iostream>
 using namespace std;
-
 int main() {
-	struct student {
-		string surname;
-		string name;
-		int age;
-		string group;
-		int id;
-	};
+	int rows, cols;
 
-	student mass[4];
-	mass[0].surname = "Kuznetsov";
-	mass[0].name = "Kirill";
-	mass[0].age = 19;
-	mass[0].group = "23vvv1";
-	mass[0].id = 489674;
+	cout << "enter rows: ";
+	cin >> rows;
+	cout << "enter cols: ";
+	cin >> cols;
 
-	mass[1].surname = "Mishanina";
-	mass[1].name = "Anna";
-	mass[1].age = 18;
-	mass[1].group = "23vvv2";
-	mass[1].id = 497674;
+	int** mass = (int**)malloc(rows * sizeof(int*));
+	for (int i = 0; i < rows; i++) {
+		mass[i] = (int*)malloc(cols * sizeof(int));
+	}
 
-	mass[2].surname = "Balaev";
-	mass[2].name = "Bleb";
-	mass[2].age = 52;
-	mass[2].group = "23vvv3";
-	mass[2].id = 409875;
-
-	mass[3].surname = "Belyakova";
-	mass[3].name = "Dariya";
-	mass[3].age = 16;
-	mass[3].group = "23vvv4";
-	mass[3].id = 525252;
-
-	int search;
-
-	cout << "select a search option surname/name/age/group/student id [1/2/3/4/5]: ";
-	cin >> search;
-
-	string target;
-	int targetInt;
-
-	if (search == 1)
-	{
-		cout << "Enter surname: ";
-		cin >> target;
-
-		for (int i = 0; i < 4; i++) {
-			if (mass[i].surname == target) {
-				cout << "Student info: " << mass[i].surname << " " << mass[i].name << " " << mass[i].age << " " << mass[i].group << " " << mass[i].id;
-			}
+	srand(time(NULL));
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			mass[i][j] = rand() % 5;
 		}
-
-		return 0;
 	}
-	else if (search == 2)
-	{
-		cout << "Enter name: ";
-		cin >> target;
 
-		for (int i = 0; i < 4; i++) {
-			if (mass[i].name == target) {
-				cout << "Student info: " << mass[i].surname << " " << mass[i].name << " " << mass[i].age << " " << mass[i].group << " " << mass[i].id;
-			}
+	cout << "\nmass:";
+	for (int i = 0; i < rows; i++) {
+		cout << "\n";
+		for (int j = 0; j < cols; j++) {
+			cout << mass[i][j] << " ";
 		}
-
-		return 0;
 	}
+	cout << endl;
+	int rowSUM;
 
-	else if (search == 3)
-	{
-		cout << "Enter age: ";
-		cin >> targetInt;
-
-		for (int i = 0; i < 4; i++) {
-			if (mass[i].age == targetInt) {
-				cout << "Student info: " << mass[i].surname << " " << mass[i].name << " " << mass[i].age << " " << mass[i].group << " " << mass[i].id;
-			}
+	/*for (int i = 0; i < rows; i++) {
+		rowSUM = 0;
+		for (int j = 0; j < cols; j++) {
+			rowSUM += mass[i][j];
 		}
-
-		return 0;
-	}
-
-	else if (search == 4)
-	{
-		cout << "Enter group: ";
-		cin >> target;
-
-		for (int i = 0; i < 4; i++) {
-			if (mass[i].group == target) {
-				cout << "Student info: " << mass[i].surname << " " << mass[i].name << " " << mass[i].age << " " << mass[i].group << " " << mass[i].id;
-			}
+		cout << "Sum " << i + 1 << " row: " << rowSUM << endl;
+	}*/
+	int SUM = 0;
+	for (int i = 0; i < rows -1; i++) {
+		
+		for (int j = i + 1; j < cols; j++) {
+			SUM += mass[i][j];
 		}
-
-		return 0;
+		
 	}
+	
+	//rows - строки
+	cout << "Sum " << SUM << endl;
+	
+	cout << endl;
+	int colSUM;
 
-	else if (search == 5)
-	{
-		cout << "Enter id: ";
-		cin >> targetInt;
-
-		for (int i = 0; i < 4; i++) {
-			if (mass[i].id == targetInt) {
-				cout << "Student info: " << mass[i].surname << " " << mass[i].name << " " << mass[i].age << " " << mass[i].group << " " << mass[i].id;
-			}
-
+	for (int j = 0; j < cols; j++) {
+		colSUM = 0;
+		for (int i = 0; i < rows; i++) {
+			colSUM += mass[i][j];
 		}
-
-		return 0;
+		cout << "Sum " << j + 1 << "col: " << colSUM << endl;
 	}
 
-	else if (search != 1 || search != 2 || search != 3 || search != 4 || search != 5)
-	{
-		cout << "Enter 1 - 5";
+	for (int i = 0; i < rows; ++i) {
+		free(mass[i]);
 	}
-
+	free(mass);
 
 	return 0;
+	
 }
