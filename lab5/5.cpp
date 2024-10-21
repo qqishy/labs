1c3dd58f0c68ee345e9a51e784576257b57531a8
qqishy <vnnv2005@mail.ru>
2024-10-22 00:08:38 +0300

    1 and 2

---
+++ lab5/5.cpp
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    int m = 5, Gsize = 0;
+    int** G, * deg;
+    setlocale (LC_ALL,"Rus");
+    printf("1task\n");
+
+    G = (int**)malloc(m * sizeof(int*));
+    for (int i = 0; i < m; i++) {
+        G[i] = (int*)malloc(m * sizeof(int));
+    }
+
+    deg = (int*)malloc(m * sizeof(int));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            G[i][j] = rand() % 2;
+            G[j][i] = G[i][j];
+            G[i][i] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", G[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n2task\n");
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (G[i][j] == 1) {
+                Gsize++;
+            }
+        }
+    }
+    Gsize = Gsize / 2;
+    printf("Size Graph: %d\n\n", Gsize);
+
+    printf("3task\n");
+
+    for (int i = 0; i < m; i++) {
+        deg[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (G[i][j] == 1) {
+                deg[i]++;
+            }
+        }
+        printf("deg: vershne %d = %d\n", i + 1, deg[i]);
+    }
+}
